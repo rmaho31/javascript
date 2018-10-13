ef3067a3ab855d463b3c605e4602ac5e3b691010
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-12 20:36:43 -0400

    first

---
+++ ClassSandbox/rectangle.js
@@ -0,0 +1,25 @@
+class Rectangle {
+    height = 15;
+    width = 10;
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+    // Getter
+    get area() {
+      return this.calcArea();
+    }
+    get heights() {
+        return this.height;
+    }
+    // Method
+    calcArea() {
+      return this.height * this.width;
+    }
+  }
+const square = new Rectangle();
+console.log(square.area);
+console.log(square.height);
+
+console.log(square.area); // 100
+
